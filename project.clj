0b14709889cf12aeb603d805c6fd325b4d0707c2
Erik Assum <erik.assum@ardoq.com>
2021-02-09 11:13:51 +0100

    Setup build for circle

--- project.clj
+++ project.clj
@@ -1,21 +1,22 @@
-(defproject com.cemerick/friend "0.3.0-SNAPSHOT"
+(defproject clj-commons/friend (or (System/getenv "PROJECT_VERSION") "0.3.0-SNAPSHOT")
   :description "Authentication and authorization library for Ring Clojure web apps and services."
-  :url "http://github.com/cemerick/friend"
+  :url "http://github.com/clj-commons/friend"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases true}]]
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring-core "1.9.0"]
                  [slingshot "0.12.2"]
-
                  [org.mindrot/jbcrypt "0.4"]
 
                  ;; http-basic
                  [commons-codec "1.15"]]
-  
-  :deploy-repositories {"releases" {:url "https://clojars.org/repo/" :creds :gpg}
-                        "snapshots" {:url "https://clojars.org/repo/" :creds :gpg}}
-  
+
   :profiles {:dev {:dependencies [[ring-mock "0.1.1"]
                                   [compojure "1.1.5"]
                                   [ring "1.2.0"]
